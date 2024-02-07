bf73e5e6dfbb07625ae347648f6a1072b3256b71
Hernan Soria <hernansoria02@gmail.com>
2024-02-07 18:32:57 -0300

    se dio formato al documento

--- js/ej2.js
+++ js/ej2.js
@@ -8,14 +8,11 @@ do {
 } while (respuesta === true);
 
 document.write(`El arreglo tiene ${ciudades.length} elemento/s`);
-ciudades.push("Paris")
-ciudades.splice(1,0,"Barcelona")
+ciudades.push("Paris");
+ciudades.splice(1, 0, "Barcelona");
 
-let UltiPosicionUsuario = ciudades[ciudades.length - 2]
-let UltiPosicionParis = ciudades[ciudades.length - 1]
-
-
-
+let UltiPosicionUsuario = ciudades[ciudades.length - 2];
+let UltiPosicionParis = ciudades[ciudades.length - 1];
 
 document.write(` <ul>
 <li>Elemento primer posicion: ${ciudades[0]}</li>
@@ -23,9 +20,9 @@ document.write(` <ul>
 <li>Elemento ultima posicion: ${UltiPosicionUsuario}</li><br>
 <li>Elemento ultima posicion: ${UltiPosicionParis}</li>
 
-</ul>`)
+</ul>`);
 
-document.write("<h1>Arreglo de ciudades</h1>")
+document.write("<h1>Arreglo de ciudades</h1>");
 document.write(`<ul>`);
 for (let indice = 0; indice < ciudades.length; indice++) {
   document.write(`<li>Elemento: ${ciudades[indice]}</li>`);
