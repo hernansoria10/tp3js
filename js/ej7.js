60333e3285882789d3398f42d83f7f3d534d8fa2
Hernan Soria <hernansoria02@gmail.com>
2024-02-10 13:38:30 -0300

    se realizo el ejercicio 7 del tp3 de arrays y funciones

---
+++ js/ej7.js
@@ -0,0 +1,13 @@
+let Numero = parseFloat(prompt("Digite un numero"))
+function TablaMultiplicar(numero){
+
+for(let i=1; i<=10; i++){
+    while(isNaN(Numero)){
+        alert('no')
+        Numero = parseFloat(prompt("por favor solo digite numeros"))
+    }
+    let multiplicacion = Numero*i
+    document.write(`${Numero}x${i} = ${multiplicacion}<br>`)
+}
+}
+TablaMultiplicar(Numero)
