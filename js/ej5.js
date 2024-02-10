cf43f257572b91937867e2d176d6abe96231b66f
Hernan Soria <hernansoria02@gmail.com>
2024-02-09 22:39:21 -0300

    se realizo el ejercicio 5 del tp3

---
+++ js/ej5.js
@@ -0,0 +1,14 @@
+let Cadena = prompt('ingrese una cadena de texto')
+function detectarMayMin(cadenaAnalizar){
+    if(Cadena == Cadena.toLocaleUpperCase()){
+        document.write(`La cadena "${Cadena}" esta formada solo por mayusculas `)
+    }
+    else if (Cadena == Cadena.toLocaleLowerCase()){
+        document.write(`La cadena "${Cadena}" esta formada solo por minusculas `)
+    }
+    else{
+        document.write(`La cadena "${Cadena}" esta formada por mayusuclas y minusculas`)
+    }
+
+}
+detectarMayMin(Cadena)
