3fd5c6bc3d28f5c56ae0209e32169d6c61b6d04b
Hernan Soria <hernansoria02@gmail.com>
2024-02-11 19:39:59 -0300

    se cambio la formula por un erros al comitear

--- js/ej6.js
+++ js/ej6.js
@@ -1,8 +1,7 @@
 let base = parseFloat(prompt("Ingrese la base"))
 let altura = parseFloat(prompt("Ingrese la altura "))
 let perimetro = 2*(altura+base)
-let perimetroCuadrado = altura + base + altura + base
-function CalcularPerimetro(formula){
+let perimetroCuadrado = 2*altura + 2*base
     if(base ===altura){
         document.write(`el perimetro del CUADRADO es: ${perimetroCuadrado} `)
     }
